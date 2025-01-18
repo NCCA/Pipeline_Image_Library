cd30356b9019b2acb8f7b862923cbcb4411ddd01
Flora9822 <Florare9822@gmail.com>
2025-01-18 01:21:45 +0000

    Update World.h

--- include/World.h
+++ include/World.h
@@ -11,8 +11,8 @@ public:
     void addRigidBody(std::unique_ptr<RigidBody> body);
     void simulate(float deltaTime);
     void printBodies() const;
-    size_t getRigidBodyCount() const;  // 提供刚体数量
-    const std::vector<std::unique_ptr<RigidBody>>& getBodies() const;  // 获取刚体列表
+    size_t getRigidBodyCount() const;  // Number of rigid bodies provided
+    const std::vector<std::unique_ptr<RigidBody>>& getBodies() const;  // Get a list of rigid bodies
 
 private:
     std::vector<std::unique_ptr<RigidBody>> bodies;
